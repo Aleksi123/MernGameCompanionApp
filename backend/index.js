9dda3c92eeeed8d39aa88f56883fa1020899c84b
Aleksi <a12345@live.fi>
2021-10-03 11:25:55 +0300

    Connection to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB, () => {
+  console.log("connected to mongodb");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
